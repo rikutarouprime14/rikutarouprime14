a9dbfb28526d60b038234a8df8ac2aec109b3819
Rikutarou Prime <93821044+rikutarouprime14@users.noreply.github.com>
2022-04-10 10:41:46 +0700

    Create Date.php

---
+++ Date.php
@@ -0,0 +1,23 @@
+<?php
+
+    function nama_hari($date){
+
+        $hari=["Ahad","Senin", "Selasa","Rabu", 'Kamis', "Jumat", "Sabtu"] 
+
+        
+
+
+
+
+
+    }
+
+
+
+    $tgl=date_create("2020-04-08");
+
+    echo nama_hari($tgl) 
+
+
+
+
